f35c657d3e7cc9df1d572c8c3a1213b3349ace37
Jingwen Huang <huajingw@sheridancollege.ca>
2023-05-23 14:20:51 -0400

    added part time student class

---
+++ src/arraydemos23tuepm/PartTimeStudent.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemos23tuepm;
+
+/**
+ *
+ * @author Jingwen
+ */
+public class PartTimeStudent {
+    //added a class
+    
+}
